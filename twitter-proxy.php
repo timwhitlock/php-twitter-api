69fed61e17dc9cf91d9674a3a6c1761cee2dd935
Tim Whitlock <r-github@timwhitlock.info>
2013-03-31 21:59:44 +0100

    added user_id and screen_name locks

--- twitter-proxy.php
+++ twitter-proxy.php
@@ -6,31 +6,54 @@
  * @return void
  */
 function proxy_user_request( $path, $ttl = 60 ){
-    
-    // Authenticate
-    $Client = new TwitterApiClient;
-    $Client->set_oauth( TW_CONSUMER_KEY, TW_CONSUMER_SEC, TW_ACCESS_KEY, TW_ACCESS_SEC );
 
-    // Twitter API params supported in GET and POST only
-    $method = strtoupper( $_SERVER['REQUEST_METHOD'] );
-    if( 'POST' === $method ){
-        $args = $_POST;
-    }
-    else {
-        $args = $_GET;
-    }
-    
-    // JSONP callback may be in request
-    if( ! empty($_REQUEST['callback']) ){
-        $callback = $_REQUEST['callback'];
-        unset( $args['callback'] );
-    }
-    
-    // execute raw api call with no caching
-    $http = $Client->raw( $path, $args, $method );
-    extract( $http );
-    $type = $headers['content-type'];
+    try {
+        
+        // default content type in case of failure
+        $type = 'application/json; charset=utf-8';
 
+        // Authenticate Twitter client from creds in config.php
+        $Client = new TwitterApiClient;
+        $Client->set_oauth( TW_CONSUMER_KEY, TW_CONSUMER_SEC, TW_ACCESS_KEY, TW_ACCESS_SEC );
+    
+        // Twitter API params supported in GET and POST only
+        $method = strtoupper( $_SERVER['REQUEST_METHOD'] );
+        if( 'POST' === $method ){
+            $args = $_POST;
+        }
+        else {
+            $args = $_GET;
+        }
+        
+        // Lock screen_name and user_id params if specified in config
+        // This is a security measure to stop other people using your proxy
+        //
+        if( TW_LOCK_USER_NAME && isset($args['screen_name']) && strcasecmp(TW_LOCK_USER_NAME, $args['screen_name']) ){
+            throw new Exception( 'Proxy locked to screen_name '.TW_LOCK_USER_NAME, 403 );
+        }
+        if( TW_LOCK_USER_ID && isset($args['user_id']) && TW_LOCK_USER_ID !== $args['user_id'] ){
+            throw new Exception( 'Proxy locked to user_id '.TW_LOCK_USER_ID, 403 );
+        }
+        
+        // JSONP callback may be in request
+        if( ! empty($_REQUEST['callback']) ){
+            $callback = $_REQUEST['callback'];
+            unset( $args['callback'] );
+        }
+        
+        // execute raw api call with no caching
+        $http = $Client->raw( $path, $args, $method );
+        extract( $http );
+        $type = $headers['content-type'];
+
+    }
+    catch( Exception $Ex ){
+        $status = $Ex->getCode();
+        $error  = array( 'message' => $Ex->getMessage() );
+        $body   = json_encode( array('errors' => array($error) ) );
+    }
+
+    
     // wrap JSONP callback function as long as response is JSON
     if( isset($callback) && 0 === strpos( $type, 'application/json' ) ){
         $type = 'text/javascript; charset=utf-8';
